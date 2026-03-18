42aca800eb706532998e714efd688f3b5f0aa059
Spider-webs <spideyboy727ai@gmail.com>
2026-03-18 11:50:26 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,10 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10743630
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+<script>
+  atOptions = {
+    'key' : '4691045a6c5bb007ca0789eaaacdda2c',
+    'format' : 'iframe',
+    'height' : 250,
+    'width' : 300,
+    'params' : {}
+  };
+</script>
+<script src="https://www.highperformanceformat.com/4691045a6c5bb007ca0789eaaacdda2c/invoke.js"></script>
